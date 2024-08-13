cbe1ef34e54543a276df5df2863f65ebd17e550f
ayyubgithub <127136465+ayyubgithub@users.noreply.github.com>
2024-08-13 19:41:33 +0530

    Update addTwoNumber.c

--- addTwoNumber.c
+++ addTwoNumber.c
@@ -1,7 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Welcome Mohd Faruk in home twon.");
+int main() {
+    int num1, num2, sum;
+
+    // Input two integers from the user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Add the two numbers
+    sum = num1 + num2;
+
+    // Display the sum
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+
     return 0;
 }
